c50ea227a29ed6e5fcf44dc843e27c51cd379184
William Readyhough <william.readyhough@gmail.com>
2020-06-10 10:30:37 -0400

    Done

---
+++ TheProgram/TheProgram/Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+//The main file to Chris's bitch ass program. Fuck I wanna die
+//Author: Will Readyhough
+//Date: 6/9/2020
+int main() {
+	cout << "Hello World" << endl;
+	return 0;
+}
